30862c7d690607f207ca9c1821071bb6685a89c2
Andrei J. Zuse <andrei@gamatec.com.br>
2013-04-20 09:35:28 -0300

    Criando classe de conexao

--- Resources/FormLogin.js
+++ Resources/FormLogin.js
@@ -1,4 +1,5 @@
 Ti.include('Funcoes.js');
+Ti.include('Conexao.js');
 
 function FormLogin()
 {
@@ -97,25 +98,7 @@ function FormLogin()
 		});
 		
 		btnLogin.addEventListener('click', function(e){
-			var url = "http://192.168.1.9:90?usuario=" + edtUsuario.getValue() + "&senha=" + edtSenha.getValue();			
- 			var client = Ti.Network.createHTTPClient({
-     			// function called when the response data is available
-     			onload : function(e) {
-         			Ti.API.info("Received text: " + this.responseText);					         			
-     			},
-    		 	// function called when an error occurs, including a timeout
-     			onerror : function(e) {
-     				msgBox('Atenção', 'Ocorreu um problema na comunicação com o servidor. Verifique sua conexão com a internet ou tente novamente mais tarde.');
-         			Ti.API.debug(e.error);
-    	    	},
-     			timeout : 5000  // in milliseconds
- 			});
- 			// Prepare the connection.
-	 		client.open("GET", url);
- 			// Send the request.
- 			client.send();  
- 					
-
+			var Conexao = new ConexaoServidor(edtUsuario.value, edtSenha.value);
 		});
 		
 		scroll.add(imgUnochapeco);
